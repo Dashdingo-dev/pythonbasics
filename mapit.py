7e294eca5b14de8c66f2aef9cd52d7d1b2fc4f30
Rokked <skylerbradley1@gmail.com>
2021-08-06 20:52:29 -0700

    new note i got things done

---
+++ mapit.py
@@ -0,0 +1,14 @@
+#! python3
+#mapit.py - launches map
+import webbrowser, sys, pyperclip
+
+#sys.argv # ['mapit.py', '870', 'Valencia', 'St.']
+#check if arguements are passed
+if len(sys.argv) > 1:
+    address = ' '.join(sys.argv[1:])
+	
+else:
+    address = pyperclip.paste()
+
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
